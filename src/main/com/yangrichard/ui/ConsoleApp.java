c56e92bbeb8abc8035d90b4cf6e13ac6ac653567
Richard Yang <richard@yangrichard.com>
2020-02-11 15:33:49 -0800

    Add entry point for console user interface

--- src/main/com/yangrichard/ui/ConsoleApp.java
+++ src/main/com/yangrichard/ui/ConsoleApp.java
@@ -1,6 +1,59 @@
 package com.yangrichard.ui;
 
+import java.util.List;
+
 // User interface when run from console
 public class ConsoleApp {
+
+    // EFFECTS: proceed with operations if arguments are provided, or exit
+    public ConsoleApp(List<String> args) {
+        if (args.size() > 0) {
+            determineOperation(args);
+        } else {
+            System.out.println("No arguments");
+        }
+    }
+
+    // EFFECTS: determines operation from argument list and calls appropriate helper method
+    private void determineOperation(List<String> args) {
+        String operation = args.get(0);
+        args.remove(0);
+        switch (operation) {
+            case "-e":
+                handleEncryption(args);
+                break;
+            case "-d":
+                handleDecryption(args);
+                break;
+            case "-u":
+                handleUpdate(args);
+                break;
+            case "-c":
+                handleCreateVault(args);
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+    }
+
+    // EFFECTS: handles encryption with provided argument list
+    private void handleEncryption(List<String> encryptionArgs) {
+
+    }
+
+    // EFFECTS: handles decryption with provided argument list
+    private void handleDecryption(List<String> decryptionArgs) {
+
+    }
+
+    // EFFECTS: handles changing password of file/vault with provided argument list
+    private void handleUpdate(List<String> updateArgs) {
+
+    }
+
+    // EFFECTS: handles creation of vault with provided argument list
+    private void handleCreateVault(List<String> createVaultArgs) {
+
+    }
 
 }
--- src/main/com/yangrichard/ui/Main.java
+++ src/main/com/yangrichard/ui/Main.java
@@ -1,10 +1,18 @@
 package com.yangrichard.ui;
 
+import java.awt.*;
+import java.util.Arrays;
+
 // Entry point into program
 public class Main {
 
+    // EFFECTS: start GUI if graphics environment is available, or run console interface otherwise
     public static void main(String[] args) {
-
+        if (GraphicsEnvironment.isHeadless()) {
+            System.exit(0);                 // no GUI implemented yet
+        } else {
+            new ConsoleApp(Arrays.asList(args));    // start console interface
+        }
     }
 
 }
