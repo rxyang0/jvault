a182802717d31a478484edecf88ec1eda12d9712
Richard Yang <richard@yangrichard.com>
2020-02-11 15:50:12 -0800

    Implement VaultEntry abstract class

--- src/main/com/yangrichard/filesystem/VaultEntry.java
+++ src/main/com/yangrichard/filesystem/VaultEntry.java
@@ -3,4 +3,20 @@ package com.yangrichard.filesystem;
 // Contains fields and methods common to all entries in a vault filesystem (e.g. files, directories)
 public abstract class VaultEntry {
 
+    private String path;
+    protected String name;
+    protected int size;     // in bytes
+
+    public String getName() {
+        return name;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
 }
