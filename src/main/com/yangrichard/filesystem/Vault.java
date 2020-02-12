2a89ae05c333c669ff85c592f38bd4aad62a54d6
Richard Yang <richard@yangrichard.com>
2020-02-12 12:44:45 -0800

    Add stubs for encrypt and decrypt helpers in Vault

--- src/main/com/yangrichard/filesystem/Vault.java
+++ src/main/com/yangrichard/filesystem/Vault.java
@@ -1,19 +1,33 @@
 package com.yangrichard.filesystem;
 
+import java.io.*;
+
 // Handles all filesystem entries and functionality in a vault
 public class Vault {
 
-    private String pathToVault;
+    private File vaultFolder;
     private VaultDirectory root;
 
     // EFFECTS: creates new vault filesystem with path in host filesystem and empty root
-    public Vault(String pathToVault) {
-        this.pathToVault = pathToVault;
-        root = new VaultDirectory("Root");
+    public Vault(File vaultFolder) {
+        this.vaultFolder = vaultFolder;
+        root = new VaultDirectory("Root", "Root");
     }
 
-    public String getPathToVault() {
-        return pathToVault;
+    // MODIFIES: dir
+    // EFFECTS: add encrypted contents of input file to vault directory
+    public void addFile(File inputFile, VaultDirectory dir) {
+        String fileName = inputFile.getName();
+        String extension = fileName.substring(fileName.lastIndexOf('.'));
+        VaultFile file = new VaultFile(fileName, fileName, extension, (int) inputFile.length());
+        // Encrypt and set reference to saved file
+        dir.addEntry(file);
+    }
+
+    // EFFECTS: decrypts and saves contents of file in vault to output directory
+    public void saveFile(VaultFile file, File outputDir) {
+        File encrypted = new File(vaultFolder.getAbsolutePath() + "/" + root.getPathOfEntry(file));
+        // Decrypt and save to outputDir
     }
 
 }
