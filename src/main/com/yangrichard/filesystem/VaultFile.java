c36a2c4d8ee8b4a5dc9e0aa79ef2cd29e2e57d06
Richard Yang <richard@yangrichard.com>
2020-02-11 17:31:30 -0800

    Implement VaultFile class and add tests

--- src/main/com/yangrichard/filesystem/VaultFile.java
+++ src/main/com/yangrichard/filesystem/VaultFile.java
@@ -3,4 +3,17 @@ package com.yangrichard.filesystem;
 // Represents a file entry in the vault filesystem
 public class VaultFile extends VaultEntry {
 
+    private String extension;   // without period
+
+    // EFFECTS: constructs VaultFile with name, extension, and size
+    public VaultFile(String name, String extension, int size) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
 }
---
+++ src/test/com/yangrichard/filesystem/VaultFileTest.java
@@ -0,0 +1,29 @@
+package com.yangrichard.filesystem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VaultFileTest {
+
+    private static final String FILE_NAME = "test";
+    private static final String FILE_EXTENSION = "txt";
+    private static final int FILE_SIZE = 1024;
+
+    private VaultFile file;
+
+    @BeforeEach
+    public void runBefore() {
+        file = new VaultFile(FILE_NAME, FILE_EXTENSION, FILE_SIZE);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(FILE_NAME, file.getName());
+        assertEquals(FILE_EXTENSION, file.getExtension());
+        assertEquals(FILE_SIZE, file.size());
+        assertNotNull(file.getId());
+    }
+
+}
