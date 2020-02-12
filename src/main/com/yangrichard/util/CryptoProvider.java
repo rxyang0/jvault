bfd9a975e831fcda65c798f318df86fa0d3f18f4
Richard Yang <richard@yangrichard.com>
2020-02-11 18:46:26 -0800

    Add constructor to CryptoProvider class

--- src/main/com/yangrichard/util/CryptoProvider.java
+++ src/main/com/yangrichard/util/CryptoProvider.java
@@ -1,6 +1,20 @@
 package com.yangrichard.util;
 
+import javax.crypto.*;
+import java.security.NoSuchAlgorithmException;
+
 // Handles all encryption and decryption processes
 public class CryptoProvider {
+
+    private static final String CIPHER_METHOD = "AES/GCM/NoPadding";
+
+    private char[] password;
+    private Cipher cipher;
+
+    // EFFECTS: stores password and instantiates new cipher
+    public CryptoProvider(char[] password) throws NoSuchPaddingException, NoSuchAlgorithmException {
+        this.password = password;
+        cipher = Cipher.getInstance(CIPHER_METHOD);
+    }
 
 }
