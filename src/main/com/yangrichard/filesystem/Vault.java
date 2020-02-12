0368afb2e4775a76bc7c9983ecd9da533f7b45de
Richard Yang <richard@yangrichard.com>
2020-02-12 15:22:21 -0800

    Add stubs for vault unlock and lock

--- src/main/com/yangrichard/filesystem/Vault.java
+++ src/main/com/yangrichard/filesystem/Vault.java
@@ -5,13 +5,30 @@ import java.io.*;
 // Handles all filesystem entries and functionality in a vault
 public class Vault {
 
-    private File vaultFolder;
+    private File vault;
     private VaultDirectory root;
+    private char[] password;
 
     // EFFECTS: creates new vault filesystem with path in host filesystem and empty root
-    public Vault(File vaultFolder) {
-        this.vaultFolder = vaultFolder;
-        root = new VaultDirectory("Root", "Root");
+    public Vault(File vaultFolder, char[] password) {
+        vault = vaultFolder;
+        if (! vaultFolder.exists()) {
+            boolean mkdirs = vault.mkdirs();
+            root = new VaultDirectory("Root", "Root");
+        } else {
+            boolean success = unlock(password);
+        }
+    }
+
+    // MODIFIES: this
+    private boolean unlock(char[] password) {
+        return false;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: locks vault by clearing password
+    private void lock() {
+        password = null;
     }
 
     // MODIFIES: dir
@@ -26,7 +43,7 @@ public class Vault {
 
     // EFFECTS: decrypts and saves contents of file in vault to output directory
     public void saveFile(VaultFile file, File outputDir) {
-        File encrypted = new File(vaultFolder.getAbsolutePath() + "/" + root.getPathOfEntry(file));
+        File encrypted = new File(vault.getAbsolutePath() + "/" + root.getPathOfEntry(file));
         // Decrypt and save to outputDir
     }
 
