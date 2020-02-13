fc1777eed42ddd0127816a8373e23d2b279511c0
Richard Yang <richard@rxyang.ca>
2020-02-12 20:21:46 -0800

    Remove console encrypt, decrypt, update operations

--- src/main/ui/ConsoleApp.java
+++ src/main/ui/ConsoleApp.java
@@ -12,9 +12,6 @@ public class ConsoleApp {
     /*
      * Parameter specification:
      *      -p  <password>                                                  specifies password (required)
-     *      -e  <path to input file>  -o <path to output file>              encrypt input file and save to output file
-     *      -d  <path to input file>  -o <path to output file>              decrypt input file and save to output file
-     *      -u  <path to file or vault directory> <new password>            re-encrypt file with new password
      *      -c  <path to new vault>                                         create new vault
      *
      *      -l  <path to vault>                                             loads existing vault for following options
@@ -32,9 +29,6 @@ public class ConsoleApp {
         } else if (! params.hasOption("p")) {
             System.out.println("No password specified");
             System.exit(1);
-        } else if ((params.hasOption("e") || params.hasOption("d")) && !params.hasOption("o")) {
-            System.out.println("No output file specified");
-            System.exit(1);
         } else if ((params.hasOption("a") || (params.hasOption("d")) || (params.hasOption("s")))
                 && !params.hasOption("l")) {
             System.out.println("No vault specified");
@@ -50,11 +44,6 @@ public class ConsoleApp {
         Option output = new Option("o", true,"output");
 
         OptionGroup operations = new OptionGroup();
-        operations.addOption(new Option("e", true, "encrypt"));
-        operations.addOption(new Option("d", true, "decrypt"));
-        Option update = new Option("u", true, "update");
-        update.setArgs(2);
-        operations.addOption(update);
         operations.addOption(new Option("c", true, "create"));
         operations.addOption(new Option("l", true, "load"));
 
@@ -86,16 +75,7 @@ public class ConsoleApp {
 
     // EFFECTS: handle specified operation from arguments
     private void determineOperation(CommandLine params) {
-        if (params.hasOption("e")) {
-            handleEncrypt(params.getOptionValue("p").toCharArray(), params.getOptionValue("e"),
-                    params.getOptionValue("o"));
-        } else if (params.hasOption("d")) {
-            handleDecrypt(params.getOptionValue("p").toCharArray(), params.getOptionValue("d"),
-                    params.getOptionValue("o"));
-        } else if (params.hasOption("u")) {
-            handleUpdate(params.getOptionValue("p").toCharArray(), params.getOptionValues("u")[0],
-                    params.getOptionValues("u")[1].toCharArray());
-        } else if (params.hasOption("c")) {
+        if (params.hasOption("c")) {
             handleCreateVault(params.getOptionValue("p").toCharArray(), params.getOptionValue("c"));
         } else if (params.hasOption("l")) {
             Vault vault = handleLoadVault(params.getOptionValue("p").toCharArray(), params.getOptionValue("l"));
@@ -125,18 +105,6 @@ public class ConsoleApp {
         } else {
             System.out.println("No operation for vault");
         }
-    }
-
-    // EFFECTS: handles encryption with provided argument list
-    private void handleEncrypt(char[] password, String inputPath, String outputPath) {
-    }
-
-    // EFFECTS: handles decryption with provided argument list
-    private void handleDecrypt(char[] password, String inputPath, String outputPath) {
-    }
-
-    // EFFECTS: handles changing password of file/vault with provided argument list
-    private void handleUpdate(char[] password, String inputPath, char[] newPassword) {
     }
 
     // EFFECTS: handles creation of vault with provided argument list
