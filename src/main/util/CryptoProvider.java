7faf836c5f5b91faf4920ea4d50f612544fc6e22
Richard Yang <richard@rxyang.ca>
2020-02-12 19:24:11 -0800

    Refactor CryptoProvider encryption operations

--- src/main/util/CryptoProvider.java
+++ src/main/util/CryptoProvider.java
@@ -13,75 +13,69 @@ import java.security.spec.InvalidKeySpecException;
 // Handles all encryption and decryption processes
 public class CryptoProvider {
 
-    private static final String KEY_METHOD = "PBKDF2WithHmacSHA256";
-    private static final int ITERATION_COUNT = 100000;
-    private static final int KEY_LENGTH = 256;
-    private static final int SALT_LENGTH = 16;
-    private static final String CIPHER_METHOD = "AES/GCM/NoPadding";
-    private static final int GCM_TAG_LENGTH = 128;
-    private static final int IV_LENGTH = 16;
+    public static final String KEY_METHOD = "PBKDF2WithHmacSHA256";
+    public static final int ITERATION_COUNT = 100000;
+    public static final int KEY_LENGTH = 256;
+    public static final int SALT_LENGTH = 16;
+    public static final String CIPHER_METHOD = "AES/GCM/NoPadding";
+    public static final int GCM_TAG_LENGTH = 128;
+    public static final int IV_LENGTH = 16;
 
     private char[] password;
-    private byte[] lastSaltAndIV;
+    private byte[] salt;
     private Cipher cipher;
 
-    // EFFECTS: stores password and instantiates new cipher
+    // EFFECTS: stores password and generates a random salt, then instantiates new cipher
     public CryptoProvider(char[] password) throws NoSuchPaddingException, NoSuchAlgorithmException {
-        setPassword(password);
+        this.password = password;
+        this.salt = generateSecureBytes(SALT_LENGTH);
         cipher = Cipher.getInstance(CIPHER_METHOD);
     }
 
-    // EFFECTS: uses a generated secret key to encrypt binary data with AES-GCM
+    // EFFECTS: stores password and salt, then instantiates new cipher
+    public CryptoProvider(char[] password, byte[] salt) throws NoSuchPaddingException, NoSuchAlgorithmException {
+        this.password = password;
+        this.salt = salt;
+        cipher = Cipher.getInstance(CIPHER_METHOD);
+    }
+
+    // EFFECTS: generates random IV, encrypts input data, then prepends IV
     public byte[] encrypt(byte[] input) throws InvalidKeySpecException, NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        byte[] salt = generateSecureBytes(SALT_LENGTH);
         byte[] iv = generateSecureBytes(IV_LENGTH);
 
-        // Merge salt and IV into one byte array
-        lastSaltAndIV = new byte[salt.length + iv.length];
-        System.arraycopy(salt, 0, lastSaltAndIV, 0, salt.length);
-        System.arraycopy(iv, 0, lastSaltAndIV, salt.length, iv.length);
+        cipher.init(Cipher.ENCRYPT_MODE, getKey(), new GCMParameterSpec(GCM_TAG_LENGTH, iv));
+        byte[] encrypted = cipher.doFinal(input);
 
-        SecretKey key = generateKeyFromPassword(password, salt);
-        cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(GCM_TAG_LENGTH, iv));
+        // Prepend encrypted data with IV
+        byte[] data = new byte[IV_LENGTH + encrypted.length];
+        System.arraycopy(iv, 0, data, 0, IV_LENGTH);
+        System.arraycopy(encrypted, 0, data, IV_LENGTH, encrypted.length);
 
-        return cipher.doFinal(input);
+        return data;
     }
 
-    // EFFECTS: regenerates key from password and salt, and uses it with same IV to decrypt data
-    public byte[] decrypt(byte[] input, byte[] saltAndIV) throws InvalidKeySpecException,
-            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException,
-            IllegalBlockSizeException {
-        // Split saltAndIV
-        byte[] salt = new byte[SALT_LENGTH];
+    // EFFECTS: uses IV and key to decrypt input data
+    public byte[] decrypt(byte[] input) throws InvalidKeySpecException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        // Split input into IV and data
         byte[] iv = new byte[IV_LENGTH];
-        System.arraycopy(saltAndIV, 0, salt, 0, SALT_LENGTH);
-        System.arraycopy(saltAndIV, SALT_LENGTH, iv, 0, IV_LENGTH);
+        byte[] encrypted = new byte[input.length - IV_LENGTH];
+        System.arraycopy(input, 0, iv, 0, IV_LENGTH);
+        System.arraycopy(input, IV_LENGTH, encrypted, 0, encrypted.length);
 
-
-        SecretKey key = generateKeyFromPassword(password, salt);
-        cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(GCM_TAG_LENGTH, iv));
-
-        return cipher.doFinal(input);
+        cipher.init(Cipher.DECRYPT_MODE, getKey(), new GCMParameterSpec(GCM_TAG_LENGTH, iv));
+        return cipher.doFinal(encrypted);
     }
 
-    // EFFECTS: creates a key appended with a generated salt from the given password
-    protected static byte[] generateKey(char[] password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        byte[] salt = generateSecureBytes(SALT_LENGTH);
-
+    // EFFECTS: regenerates key from password and salt
+    protected SecretKey getKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
         // Define new password-based encryption specification based on these parameters
         PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATION_COUNT, KEY_LENGTH);
 
         // Use a secret-key factory to construct a key
         SecretKeyFactory skf = SecretKeyFactory.getInstance(KEY_METHOD);
-        byte[] key = skf.generateSecret(spec).getEncoded();
-
-        // Prepend key with salt
-        byte[] saltAndKey = new byte[SALT_LENGTH + key.length];
-        System.arraycopy(salt, 0, saltAndKey, 0, salt.length);
-        System.arraycopy(key, 0, saltAndKey, salt.length, key.length);
-
-        return saltAndKey;
+        return new SecretKeySpec(skf.generateSecret(spec).getEncoded(), "AES");
     }
 
     // EFFECTS: securely generates given number of random bytes
@@ -92,20 +86,14 @@ public class CryptoProvider {
     }
 
     // MODIFIES: this
-    // EFFECTS: sets password
-    protected void setPassword(char[] password) {
-        this.password = password;
-    }
-
-    // MODIFIES: this
-    // EFFECTS: sets password and lastSaltAndIV to null to protect data
+    // EFFECTS: sets password and salt to null
     public void destroy() {
         password = null;
-        lastSaltAndIV = null;
+        salt = null;
     }
 
-    public byte[] getLastSaltAndIV() {
-        return lastSaltAndIV;
+    public byte[] getSalt() {
+        return salt;
     }
 
 }
