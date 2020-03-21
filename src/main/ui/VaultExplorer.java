ed03701f1117d7bc5ba96d9c0234380c5e4af5a8
Richard Yang <richard@yangrichard.com>
2020-03-21 17:26:22 -0600

    Set layout and create stubs in VaultExplorer

--- src/main/ui/VaultExplorer.java
+++ src/main/ui/VaultExplorer.java
@@ -1,27 +1,57 @@
 package ui;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
+// Displays navigation controls, address bar, and vault file explorer
 public class VaultExplorer extends BorderPane {
 
+    private TextField address;
+    private ListView<String> list;
+
+    // EFFECTS: adds all elements to pane
     public VaultExplorer() {
         addAddressBar();
         addFileExplorer();
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds navigation controls and address bar to pane
     private void addAddressBar() {
-        Pane addressBar = new Pane();
+        HBox addressBar = new HBox();
+        addressBar.setPadding(new Insets(2, 15, 0, 10));
 
         Button back = new Button("←");
         addressBar.getChildren().add(back);
 
+        address = new TextField("No Vault Loaded");
+        address.setEditable(false);
+        address.setMouseTransparent(true);
+        HBox.setHgrow(address, Priority.ALWAYS);
+        addressBar.getChildren().add(address);
+
         this.setTop(addressBar);
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds file explorer to pane
     private void addFileExplorer() {
-
+        list = new ListView<>();
+        list.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.PRIMARY && e.getClickCount() == 2) {
+                // Double click
+            }
+        });
+        list.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            // Select item
+        });
+        this.setCenter(list);
     }
 
 }
