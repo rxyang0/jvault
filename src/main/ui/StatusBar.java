6bc1ef62203c82849b756b62b657d8577da2e4f3
Richard Yang <richard@yangrichard.com>
2020-03-21 19:27:23 -0600

    Implement show status and error methods in StatusBar

--- src/main/ui/StatusBar.java
+++ src/main/ui/StatusBar.java
@@ -2,11 +2,25 @@ package ui;
 
 import javafx.scene.control.TextField;
 
+// Displays status information
 public class StatusBar extends TextField {
 
     public StatusBar() {
         this.setEditable(false);
         this.setMouseTransparent(true);
+        showStatus("No Vault Loaded");
+    }
+
+    // EFFECTS: displays text in black
+    public void showStatus(String text) {
+        this.setStyle("-fx-text-fill: black;");
+        this.setText(text);
+    }
+
+    // EFFECTS: displays text in red
+    public void showError(String text) {
+        this.setStyle("-fx-text-fill: red;");
+        this.setText(text);
     }
 
 }
